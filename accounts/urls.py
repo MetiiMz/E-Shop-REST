bce9a01b1495cbc2b2a89df381850f6e7e2265ee
Mehdi Mz <mehdi.mousazadeh.n0@gmail.com>
2023-11-25 22:12:38 +0330

    Add JWT authentication to project and login url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 app_name = 'accounts'
 urlpatterns = [
-    path('register/', views.UserRegisterView.as_view(), name='user_register')
+    path('register/', views.UserRegisterView.as_view(), name='user_register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
